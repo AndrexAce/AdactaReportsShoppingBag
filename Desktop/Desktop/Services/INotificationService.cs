a37045755f6f964c93e895de62426713525f41f7
Andrea Lucchese <andrea@testpoint.it>
2025-11-27 11:45:43 +0100

    Aggiunta notifiche con barra di progresso.

--- Desktop/Desktop/Services/INotificationService.cs
+++ Desktop/Desktop/Services/INotificationService.cs
@@ -7,5 +7,10 @@ internal interface INotificationService
 {
     public Guid ShowNotification(string title, string message);
 
+    public Task<Guid> ShowProgressNotificationAsync(string title, string message, string statusMessage,
+        uint totalToProcess);
+
     public Task RemoveNotificationAsync(Guid tag);
+
+    public Task UpdateProgressNotificationAsync(Guid tag, string statusMessage, uint processed, uint totalToProcess);
 }
--- Desktop/Desktop/Services/NotificationService.cs
+++ Desktop/Desktop/Services/NotificationService.cs
@@ -17,7 +17,8 @@ internal class NotificationService : INotificationService
             .BuildNotification();
 
         notification.Tag = tag.ToString();
-        notification.ExpiresOnReboot = false;
+        notification.Group = tag.ToString();
+        notification.ExpiresOnReboot = true;
 
         AppNotificationManager.Default.Show(notification);
 
@@ -25,8 +26,45 @@ internal class NotificationService : INotificationService
         return tag;
     }
 
+    public async Task<Guid> ShowProgressNotificationAsync(string title, string message, string statusMessage, uint totalToProcess)
+    {
+        var tag = Guid.NewGuid();
+
+        var notification = new AppNotificationBuilder()
+            .AddText(title)
+            .AddText(message)
+            .AddProgressBar(new AppNotificationProgressBar()
+            .BindStatus()
+            .BindValue()
+            .BindValueStringOverride())
+            .BuildNotification();
+
+        notification.Tag = tag.ToString();
+        notification.Group = tag.ToString();
+        notification.ExpiresOnReboot = true;
+
+        AppNotificationManager.Default.Show(notification);
+
+        await UpdateProgressNotificationAsync(tag, statusMessage, 0, totalToProcess);
+
+        // Return the tag so caller can delete or update the notification later
+        return tag;
+    }
+
     public async Task RemoveNotificationAsync(Guid tag)
     {
-        await AppNotificationManager.Default.RemoveByTagAsync(tag.ToString()).AsTask();
+        await AppNotificationManager.Default.RemoveByTagAndGroupAsync(tag.ToString(), tag.ToString());
+    }
+
+    public async Task UpdateProgressNotificationAsync(Guid tag, string statusMessage, uint processed, uint totalToProcess)
+    {
+        var data = new AppNotificationProgressData(processed + 1)
+        {
+            Status = statusMessage,
+            Value = (double) processed / totalToProcess,
+            ValueStringOverride = $"{processed}/{totalToProcess}"
+        };
+
+        await AppNotificationManager.Default.UpdateAsync(data, tag.ToString(), tag.ToString());
     }
 }
